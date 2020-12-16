d09c96bd863138723049206a23d14610c4b0744d
Gagis <jyri.p.hamalainen@gmail.com>
2020-12-16 16:54:26 +0200

    basic data Model for Card

--- scryfall/models.py
+++ scryfall/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    name = models.CharField('Card Name', max_length=200)
+    set = models.CharField('Set Name', max_length=200)
+    collector_number = models.IntegerField('Collector Number')
+    scryfall_id = models.CharField('Scryfall ID', max_length=200)
+
+    def __str__(self):
+        return self.name
