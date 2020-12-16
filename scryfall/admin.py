81ba17dbecbbf9b88bbda0f8922dde4dc25e465e
Gagis <jyri.p.hamalainen@gmail.com>
2020-12-16 21:29:01 +0200

    Cleanup, round 1.

--- scryfall/admin.py
+++ scryfall/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models import Card
 
-
+# Jotta lisättyjä kortteja voisi poistaa tämän kautta.
 admin.site.register(Card)
--- scryfall/views.py
+++ scryfall/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 
 from .models import Card
 from .forms import IDForm
@@ -8,7 +8,7 @@ from .api import fetch
 
 
 def index(request):
-    """Indexiä ei nyt käytetä mihinkään jännään."""
+    # Indexiä ei nyt käytetä mihinkään jännään.
     return HttpResponse("Hei maailma! Kohta lisätään kortteja kantaan!")
 
 
@@ -18,14 +18,16 @@ def add(request):
     """
     if request.method == 'POST':
         # Poimin tän rakenteen formien dokumentaatiosta, mutten tiedä, miksi
-        # if POST ja else rakenne on relevantti
+        # if request.method ja else rakenne on relevantti. Voi olla että formi
+        # on tarpeettoman monimutkainen objekti vrt. request.POST käyttöön.
         form = IDForm(request.POST)
 
         if form.is_valid():
             input_id = form.cleaned_data['scryfall_id']
             if Card.objects.filter(scryfall_id=input_id).exists():
                 error = 'Kortti on jo tietokannassa.'
-
+                return render(request, 'scryfall/add.html',
+                              {'form': form, 'message': error})
             else:
                 card = fetch(input_id)
                 Card(name=card['name'],
@@ -34,7 +36,8 @@ def add(request):
                      scryfall_id=card['scryfall_id'],
                      ).save()
 
-            return HttpResponseRedirect('add/')
+            return render(request, 'scryfall/add.html',
+                          {'form': form, 'message': 'Kortti lisätty.'})
 
     else:
         form = IDForm()
